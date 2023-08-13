0a2ab54b2b29cbd57ec4b5af155b48bdc5e88abb
Justin Garner <j.garner828@gmail.com>
2023-08-13 14:20:40 -0400

    hello spring routing correctly running.

---
+++ reactive-test/src/main/java/models/Greeting.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.Objects;
+
+public class Greeting {
+
+    private String message;
+
+    public Greeting() {}
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Greeting greeting = (Greeting) o;
+        return message.equals(greeting.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(message);
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}

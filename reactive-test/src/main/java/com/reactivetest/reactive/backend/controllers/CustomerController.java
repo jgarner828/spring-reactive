90526214b409a69461c4ee0d068ec0204ecfaaa6
Justin Garner <j.garner828@gmail.com>
2023-08-24 09:54:45 -0400

    added post mapping for customers table

--- reactive-test/src/main/java/com/reactivetest/reactive/backend/controllers/CustomerController.java
+++ reactive-test/src/main/java/com/reactivetest/reactive/backend/controllers/CustomerController.java
@@ -4,9 +4,11 @@ import com.reactivetest.reactive.backend.models.Customer;
 import com.reactivetest.reactive.backend.repositories.repositories.CustomerRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class CustomerController {
@@ -23,5 +25,10 @@ public class CustomerController {
         return repository.findAll();
     }
 
+    @PostMapping("/customers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addCustomer(Customer customer) {
+        repository.save(customer);
+    }
 
 }
